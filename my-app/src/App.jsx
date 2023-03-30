9e699dca95798a87a40a0d333c1262545679e4ae
woshierha <erhahappy@gmail.com>
2023-03-30 14:35:27 +0800

    feat: 列表增加拖拽

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,11 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect} from 'react';
 import InputBox from './components/Input.jsx'
 import  Item from './components/Item.jsx'
-import { List } from 'antd'
+// import { List } from 'antd'
+import Sortable from 'sortablejs'; 
+
+// import { ReactSortable } from 'react-sortablejs'
 function App() {
   const [datalist, setDatalist] = useState([])
   const getInoutData = (data) => {
@@ -22,17 +25,44 @@ function App() {
       return i !== index
     }))
   }
+  useEffect(() => {
+    const listItems = document.querySelector('.todo-list');
+    const options = {
+      animation: 150,
+      ghostClass: 'blue-background-class',
+      handle: '.list-item',
+      onEnd: (evt) => {
+        const newIndex = evt.newIndex;
+        const oldIndex = evt.oldIndex;
+        console.log('new:',newIndex, 'old:',oldIndex)
+        if (newIndex !== oldIndex) {
+          setDatalist((prevList) => {
+            const newList = [...prevList];
+            const current = newList[oldIndex];
+            newList.splice(oldIndex, 1);
+            newList.splice(newIndex, 0, current);
+            console.log(newList)
+            return newList;
+          });
+        }
+      },
+    };
+    Sortable.create(listItems, options);
+  })
   return (
     <div className="App">
-      <h2>To-Do-List</h2>
-      <List className='todo-list'>
+      <h2 id='title'>To-Do-List</h2>
+      <div className='todo-list'>
         {datalist.map((item, index) => {
-          return <Item ondeleteData={delData} onDataChange={getChangeData} index={index} key={index} data={item}></Item> } 
+          return (
+            <div key={`${item}-${index}`} className="list-item">
+               <Item ondeleteData={delData} onDataChange={getChangeData} index={index} key={index} data={item}></Item>
+            </div>
+           )} 
         )}
-      </List>
+      </div>
       <InputBox getData={getInoutData}></InputBox>
     </div>
   );
 }
-
 export default App;
